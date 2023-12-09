ddb310d0e0ec4a8675ec3779bea1e3fe1e639aa9
Sheikah45 <Sheikah450@gmail.com>
2023-12-09 17:13:54 -0500

    Start pulling out parser

---
+++ fx2j-parser/src/main/java/io/github/sheikah45/fx2j/parser/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package io.github.sheikah45.fx2j.parser.utils;
+
+public class StringUtils {
+
+    public static boolean isNullOrBlank(String string) {
+        return string == null || string.isBlank();
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "fx2j"
 include("fx2j-api")
 include("fx2j-processor")
 include("fx2j-gradle-plugin")
+include("fx2j-parser")
